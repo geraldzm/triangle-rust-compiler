84d9f03ee345b90ef542aa8bfc1d6bd1fba6f331
Gerald Zamora <gerald.zamora.morales@intel.com>
2023-12-30 14:29:17 -0600

    Create token file

--- src/syntactic_analyzer/mod.rs
+++ src/syntactic_analyzer/mod.rs
@@ -2,3 +2,4 @@ pub mod errors;
 pub mod lexical_analyser;
 pub mod source_file;
 pub mod source_position;
+pub mod token;
--- src/syntactic_analyzer/token.rs
+++ src/syntactic_analyzer/token.rs
@@ -1,1 +1,67 @@
+use super::source_position::SourcePosition;
 
+pub struct Token {
+    kind: TokenKind,
+    spelling: String,
+    position: SourcePosition,
+}
+
+impl Token {
+    pub fn new(kind: TokenKind, spelling: String, position: SourcePosition) -> Token {
+        Token {
+            kind,
+            spelling,
+            position,
+        }
+    }
+}
+
+pub enum TokenKind {
+    // literals, identifiers, operators...
+    INTLITERAL,
+    CHARLITERAL,
+    IDENTIFIER,
+    OPERATOR,
+
+    // reserved words ...
+    ARRAY,
+    BEGIN,
+    CONST,
+    DO,
+    ELSE,
+    END,
+    FUNC,
+    IF,
+    IN,
+    LET,
+    OF,
+    PROC,
+    RECORD,
+    THEN,
+    TYPE,
+    VAR,
+    WHILE,
+
+    // punctuation...
+    DOT,
+    COLON,
+    SEMICOLON,
+    COMMA,
+    BECOMES,
+    IS,
+
+    // brackets...
+    LPAREN,
+    RPAREN,
+    LBRACKET,
+    RBRACKET,
+    LCURLY,
+    RCURLY,
+
+    // special tokens...
+    EOT,
+    ERROR,
+}
+
+#[cfg(test)]
+mod tests {}
