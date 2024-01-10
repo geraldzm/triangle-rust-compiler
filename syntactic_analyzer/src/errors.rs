ac6836e3088ac07b127d1258ceb7ec411426f53a
gerald <geraldzmt@gmail.com>
2024-01-09 20:00:04 -0600

    Add error reporter

--- syntactic_analyzer/src/errors.rs
+++ syntactic_analyzer/src/errors.rs
@@ -2,6 +2,8 @@ use std::error::Error;
 use std::fmt::Display;
 use std::fmt::Formatter;
 
+use crate::source_position::SourcePosition;
+
 #[derive(Debug)]
 pub struct AscciError;
 
@@ -22,5 +24,39 @@ impl Error for SyntaxError {}
 impl Display for SyntaxError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "Syntax Error found")
+    }
+}
+
+//
+pub struct ErrorReporter {
+    num_errors: u32,
+}
+
+impl ErrorReporter {
+    pub fn new() -> ErrorReporter {
+        ErrorReporter { num_errors: 0 }
+    }
+
+    pub fn report_error(&mut self, message: &str, token_name: &str, pos: &SourcePosition) {
+        println!("ERROR: ");
+
+        message.chars().for_each(|c| {
+            if c == '%' {
+                print!("{token_name}");
+            } else {
+                print!("{c}");
+            }
+        });
+
+        println!(" {pos}");
+        self.num_errors += 1;
+    }
+
+    pub fn report_restriction(&self, message: &str) {
+        println!("RESTRICTION: {message}");
+    }
+
+    pub fn get_number_errors(&self) -> u32 {
+        self.num_errors
     }
 }
