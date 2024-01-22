4db1b1f8800d439a4385dd30ebc34d1c56e5b157
gerald <geraldzmt@gmail.com>
2024-01-21 18:42:08 -0600

    Integer literal

--- src/compiler/src/abstract_syntax_trees/integer_literal.rs
+++ src/compiler/src/abstract_syntax_trees/integer_literal.rs
@@ -1,35 +1,12 @@
 use crate::syntactic_analyzer::source_position::SourcePosition;
 
-use super::{terminal::Terminal, ast::AST, visitor};
-
 pub struct IntegerLiteral {
     spelling: String,
-    position: SourcePosition
+    position: SourcePosition,
 }
 
-    // public IntegerLiteral (String theSpelling, SourcePosition thePosition) {
-    //     super (theSpelling, thePosition);
-    //   }
-    
-    //   public Object visit(Visitor v, Object o) {
-    //     return v.visitIntegerLiteral(this, o);
-    //   }
-
-impl Terminal for IntegerLiteral {
-    fn new(spelling: String, position: SourcePosition) -> Self {
-        IntegerLiteral {
-            spelling,
-            position
-        }
-    }
-}
-
-impl AST for IntegerLiteral {
-    fn get_position<'a>(&'a self) -> &'a SourcePosition {
-        todo!()
-    }
-
-    fn visit<T, G>(&mut self, visitor: &impl visitor::Visitor, t: T) -> G {
-        todo!()
+impl IntegerLiteral {
+    pub fn new(spelling: String, position: SourcePosition) -> Self {
+        Self { spelling, position }
     }
 }
--- src/compiler/src/abstract_syntax_trees/mod.rs
+++ src/compiler/src/abstract_syntax_trees/mod.rs
@@ -1,7 +1,7 @@
 pub mod ast;
 pub mod call_command;
-pub mod command;
 pub mod character_literal;
+pub mod command;
 pub mod identifier;
 pub mod integer_literal;
 pub mod proc_actual_parameter;
