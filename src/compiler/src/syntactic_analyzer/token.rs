3049f7bfde0579b3d82a289dd29912a890d0af83
gerald <geraldzmt@gmail.com>
2024-01-22 00:12:03 -0600

    Add spelling to tokenKind

--- src/compiler/src/syntactic_analyzer/token.rs
+++ src/compiler/src/syntactic_analyzer/token.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use super::source_position::SourcePosition;
 
 pub struct Token {
@@ -82,6 +84,54 @@ pub enum TokenKind {
     // special tokens...
     EOT,
     ERROR,
+}
+
+impl TokenKind {
+    pub fn get_spelling(&self) -> &'static str {
+        match self {
+            TokenKind::INTLITERAL => "<int>",
+            TokenKind::CHARLITERAL => "<char>",
+            TokenKind::IDENTIFIER => "<identifier>",
+            TokenKind::OPERATOR => "<operator>",
+            TokenKind::ARRAY => "array",
+            TokenKind::BEGIN => "begin",
+            TokenKind::CONST => "const",
+            TokenKind::DO => "do",
+            TokenKind::ELSE => "else",
+            TokenKind::END => "end",
+            TokenKind::FUNC => "func",
+            TokenKind::IF => "if",
+            TokenKind::IN => "in",
+            TokenKind::LET => "let",
+            TokenKind::OF => "of",
+            TokenKind::PROC => "proc",
+            TokenKind::RECORD => "record",
+            TokenKind::THEN => "then",
+            TokenKind::TYPE => "type",
+            TokenKind::VAR => "var",
+            TokenKind::WHILE => "while",
+            TokenKind::DOT => ".",
+            TokenKind::COLON => ":",
+            TokenKind::SEMICOLON => ";",
+            TokenKind::COMMA => ",",
+            TokenKind::BECOMES => ":=",
+            TokenKind::IS => "~",
+            TokenKind::LPAREN => "(",
+            TokenKind::RPAREN => ")",
+            TokenKind::LBRACKET => "[",
+            TokenKind::RBRACKET => "]",
+            TokenKind::LCURLY => "{",
+            TokenKind::RCURLY => "}",
+            TokenKind::EOT => "",
+            TokenKind::ERROR => "<error>",
+        }
+    }
+}
+
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.get_spelling())
+    }
 }
 
 #[cfg(test)]
