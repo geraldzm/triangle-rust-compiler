26751ad41e299ef8ef415e30b889d8aed36e2058
gerald <geraldzmt@gmail.com>
2024-01-21 19:09:43 -0600

    Add tree identifier and Character

--- src/compiler/src/abstract_syntax_trees/character_literal.rs
+++ src/compiler/src/abstract_syntax_trees/character_literal.rs
@@ -1,32 +1,12 @@
-
 use crate::syntactic_analyzer::source_position::SourcePosition;
-
-use super::{ast::AST, visitor::Visitor};
-
-
 
 pub struct CharacterLiteral {
     spelling: String,
-    position: SourcePosition   
+    position: SourcePosition,
 }
 
 impl CharacterLiteral {
-    fn new(spelling: String, position: SourcePosition) -> Self {
-        CharacterLiteral {
-            spelling,
-            position
-        }
+    pub fn new(spelling: String, position: SourcePosition) -> Self {
+        Self { spelling, position }
     }
-}
-
-
-impl AST for CharacterLiteral {
-    fn get_position<'a>(&'a self) -> &'a SourcePosition {
-        &self.position
-    }
-
-    fn visit<T, G>(&mut self, visitor: &impl Visitor, t: T) -> G {
-        todo!()
-    }
-
 }
--- src/compiler/src/abstract_syntax_trees/identifier.rs
+++ src/compiler/src/abstract_syntax_trees/identifier.rs
@@ -1,37 +1,12 @@
 use crate::syntactic_analyzer::source_position::SourcePosition;
 
-use super::{ast::AST,  type_denoter::TypeDenoter};
-
-
-pub struct Identifier<T: AST, U: TypeDenoter + AST> {
-    spelling: String, 
+pub struct Identifier {
+    spelling: String,
     position: SourcePosition,
-    pub typ: Option<U>,
-    pub decl: Option<T> // Either a Declaration or a FieldTypeDenoter
 }
 
-impl<T: AST, U: TypeDenoter + AST> Identifier<T, U> {
-    fn new(spelling: String, position: SourcePosition) -> Self {
-        Identifier {
-            spelling,
-            position,
-            typ: None,
-            decl: None
-        }
+impl Identifier {
+    pub fn new(spelling: String, position: SourcePosition) -> Self {
+        Self { spelling, position }
     }
 }
-
-impl<T: AST, U: TypeDenoter + AST> AST for Identifier<T, U> {
-    fn get_position<'a>(&'a self) -> &'a SourcePosition {
-        &self.position
-    }
-
-    fn visit<A, G>(&mut self, visitor: &impl super::visitor::Visitor, t: A) -> G {
-        todo!()
-    }
-
-}
-
-//     public Object visit(Visitor v, Object o) {
-//       return v.visitIdentifier(this, o);
-//     }
